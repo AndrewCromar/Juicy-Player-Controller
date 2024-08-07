853fd7560d23176c6a434abb84ee864b9c6ba166
Andrew Cromar <andrewmcromar@gmail.com>
2024-08-07 07:28:20 -0700

    fix: sorting files

---
+++ Assets/ONYX Development/Juicy-Player-Controller/Scripts/ReadOnly/ReadOnlyAttribute.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+namespace ONYX {
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
--- Scripts/ReadOnly/ReadOnlyAttribute.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-namespace ONYX {
-    public class ReadOnlyAttribute : PropertyAttribute { }
-}
